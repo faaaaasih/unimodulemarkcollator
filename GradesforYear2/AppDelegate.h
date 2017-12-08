af62581287b1535836fe6e6a082d9a6e46b7f867
Fasih Qureshi <fasihqureshi@Fasihs-MacBook-Air.local>
2017-12-08 15:03:35 +0000

    Initial Commit

---
+++ GradesforYear2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GradesforYear2
+//
+//  Created by Fasih Qureshi on 08/12/2017.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GradesforYear2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GradesforYear2
+//
+//  Created by Fasih Qureshi on 08/12/2017.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
