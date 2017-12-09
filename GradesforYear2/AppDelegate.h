417efa8a83ee7eabc348474950e3290336ea39ce
Fasih Qureshi <fasihqureshi@Fasihs-MacBook-Air.local>
2017-12-09 02:07:13 +0000

    Added customisation to app delegate from tab bar Tab bar implementation from Github Cocoapods were used

--- GradesforYear2/AppDelegate.h
+++ GradesforYear2/AppDelegate.h
@@ -7,8 +7,17 @@
 //
 
 #import <UIKit/UIKit.h>
+//model
+#import "YALTabBarItem.h"
+
+//controller
+#import "YALFoldingTabBarController.h"
+
+//helpers
+#import "YALAnimatingTabBarConstants.h"
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
+
 
 @property (strong, nonatomic) UIWindow *window;
 
--- GradesforYear2/AppDelegate.m
+++ GradesforYear2/AppDelegate.m
@@ -17,7 +17,48 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
+    
+    YALFoldingTabBarController *tabBarController = (YALFoldingTabBarController *) self.window.rootViewController;
+    
+    //prepare leftBarItems
+    YALTabBarItem *item1 = [[YALTabBarItem alloc] initWithItemImage:[UIImage imageNamed:@"album-simple-7.png"] // initial image identifier
+                                                      leftItemImage:nil // shen user clicks on initial item
+                                                     rightItemImage:nil];
+    
+    
+    YALTabBarItem *item2 = [[YALTabBarItem alloc] initWithItemImage: [UIImage imageNamed:@"book-cover-star-7.png"]
+                                                      leftItemImage: nil
+                                                     rightItemImage:nil];
+    
+    tabBarController.leftBarItems = @[item1, item2];
+    
+    
+    
+    //prepare rightBarItems
+    
+    
+    
+    YALTabBarItem *item3 = [[YALTabBarItem alloc] initWithItemImage:nil
+                                                      leftItemImage:nil
+                                                     rightItemImage:nil];
+    
+    YALTabBarItem *item4 = [[YALTabBarItem alloc] initWithItemImage:nil
+                                                      leftItemImage:nil
+                                                     rightItemImage:nil];
+
+    tabBarController.rightBarItems = @[item3, item4];
+    
+    tabBarController.tabBarView.extraTabBarItemHeight = YALExtraTabBarItemsDefaultHeight; // extra details 
+    tabBarController.tabBarView.offsetForExtraTabBarItems = YALForExtraTabBarItemsDefaultOffset;
+    tabBarController.tabBarView.backgroundColor = [UIColor colorWithRed:94.f/255.f green:91.f/255.f blue:149.f/255.f alpha:1.f];
+    tabBarController.tabBarView.tabBarColor = [UIColor colorWithRed:72.f/255.f green:211.f/255.f blue:178.f/255.f alpha:1.f];
+    tabBarController.tabBarViewHeight = YALTabBarViewDefaultHeight;
+    tabBarController.tabBarView.tabBarViewEdgeInsets = YALTabBarViewHDefaultEdgeInsets;
+    tabBarController.tabBarView.tabBarItemsEdgeInsets = YALTabBarViewItemsDefaultEdgeInsets;
+    
     return YES;
+
 }
 
 
